60f562a4f1e7f04f6b60205fd743eca3396f93f0
Riccardo Zanotto <rikyzan@gmail.com>
2018-08-07 15:11:24 +0200

    Scritta esercitazione 4

--- eqdiff/eser4.m
+++ eqdiff/eser4.m
@@ -1,36 +1,42 @@
 es=4;
 
 if es==1
-  ys=[0.1 0.3 0.5 10];
+  ys=[0.1 0.5 10];
   y0=[0.5;0.3];
   [x1,y1] = ode45(@(x,y) funLV(x,y,3),[0 300],y0);
+  subplot(2,2,1);
   plot(x1,y1(:,1),x1,y1(:,2),'r')
-  pause(5);
-  for i=1:4
+  legend('0.5', '0.3')
+  for i=1:3
     y0=[ys(i);0.5];
     [x,y] = ode45(@(x,y) funLV(x,y,3),[0 300],y0);
+    subplot(2,2,i+1)
     plot(x,y(:,1),x,y(:,2),'r')
-    pause(5);
+    legend(num2str(y0(1)),num2str(y0(2)))
   end
   
 elseif es==2
-  y0=[300;150];
-  y0=[15;22];
-  [x,y] = RK4(@(x,y) funLV(x,y,4),[0 20],y0,0.01);
-  plot(x,y(1,:),x,y(2,:),'r')
+  ys=[ [300;150] [15;22]];
+  for i=1:2
+    y0=ys(:,i);
+    [x,y] = RK4(@(x,y) funLV(x,y,4),[0 20],y0,0.01);
+    subplot(1,2,i)
+    plot(x,y(1,:),x,y(2,:),'r')
+  end
   
 elseif es==3
   y0=[7.7;0.5];
-  [x,y] = ode45(@(x,y) funLV(x,y,5),[0 25],y0);
+  [x,y] = ode45(@(x,y) funLV(x,y,5),[0 10],y0);
   subplot(1,2,1)
+  plot(y(:,1),y(:,2))
+  [x,y] = ode45(@(x,y) funLV(x,y,5),[0 25],y0);
+  subplot(1,2,2) 
   plot(x,y(:,1),x,y(:,2),'r')
-  subplot(1,2,2)
-  plot(y(:,2),y(:,1))
 
 elseif es==4
   y0 = [999;1];
   [x,y] = ode45(@funSIR,[0 100],y0);
-  plot(x,y(:,1),x,y(:,2),'r')
+  plot(x,y(:,1),x,y(:,2),'r',[0 100],[800 800],'g')
 
 end
 
