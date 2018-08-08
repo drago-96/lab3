e8340cf22a6e36e9cec98ae3b86288010224a981
Riccardo Zanotto <rikyzan@gmail.com>
2018-08-08 12:01:32 +0200

    Inviata esercitazione 5

--- eqdiff/eser5.m
+++ eqdiff/eser5.m
@@ -11,23 +11,25 @@ if es==1
   funP=@(x,y) [y(2);-k/m*y(2)-9.8];
   [x2,y2]=RK4(funP,[xp 150],yf,0.1);
   subplot(2,1,1);
-  plot(x1,y1(1,:),'b',x2,y2(1,:),'b');
+  plot(x1,y1(1,:),'b',x2,y2(1,:),'b',[0 150], [0 0], 'k');
   subplot(2,1,2);
   plot(x1,y1(2,:),'r',x2,y2(2,:),'r');
-  y2;
+  y2
   
 elseif es==2
   f = @(x,y) [y(2); -9.8/2*sin(y(1))];
   y0 = [pi/6;0];
   [x,y] = ode45(f,[0 10],y0);
   figure(1)
-  plot(x,y(:,1),x,y(:,2))
+  plot(x,y(:,1),x,y(:,2),'r')
+  legend('posizione','velocità')
 
 elseif es==3
   f = @(x,y) [y(2); -9.8/2*y(1)];
   y0 = [pi/6;0];
   [x,y] = ode45(f,[0 10],y0);
   figure(2)
-  plot(x,y(:,1),x,y(:,2))
+  plot(x,y(:,1),x,y(:,2),'r')
+  legend('posizione','velocità')
   
 end
